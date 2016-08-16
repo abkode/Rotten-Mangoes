cb59787082a85d1959703911a2a4a810cf7a97ac
abkode <abozar.rahimi@gmail.com>
2016-08-16 00:12:30 +0000

    Flash notices and alerts added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,37 +16,37 @@ class MoviesController < ApplicationController
   end
 
   def create
-	@movie = Movie.new(movie_params)
+  	@movie = Movie.new(movie_params)
 
-	if @movie.save
-	  redirect_to movies_path
-	else
-	  render :new
-	end
+  	if @movie.save
+      redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
+  	else
+  	  render :new
+  	end
   end
 
   def update
-	@movie = Movie.find(params[:id])
+  	@movie = Movie.find(params[:id])
 
-	if @movie.update_attributes(movie_params)
-	  redirect_to movie_path(@movie)
-	else
-	  render :edit
-	end
+  	if @movie.update_attributes(movie_params)
+  	  redirect_to movie_path(@movie)
+  	else
+  	  render :edit
+  	end
   end
 
   def destroy
-	@movie = Movie.find(params[:id])
-	@movie.destroy
-	redirect_to movies_path
+  	@movie = Movie.find(params[:id])
+  	@movie.destroy
+  	redirect_to movies_path
   end
 
   protected
 
   def movie_params
-	params.require(:movie).permit(
-	  :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
-	)
+  	params.require(:movie).permit(
+  	  :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
+  	)
   end
 
 end
